2b155f489e203e50f52028e965f634f66a2eff04
saka <sakanabil1221@gmail.com>
2025-03-15 17:26:26 +0700

    JS 3 Praktikum 5

--- POS/app/Http/Controllers/KategoriController.php
+++ POS/app/Http/Controllers/KategoriController.php
@@ -9,14 +9,23 @@ class KategoriController extends Controller
 {
     public function index()
     {
-        $data = [
-            'kategori_kode' => 'SNK',
-            'kategori_nama' => 'Snack/Makanan Ringan',
-            'created_at' => now()
-        ];
+        // $data = [
+        //     'kategori_kode' => 'SNK',
+        //     'kategori_nama' => 'Snack/Makanan Ringan',
+        //     'created_at' => now()
+        // ];
         
-        DB::table('m_kategori')->insert($data);
+        // DB::table('m_kategori')->insert($data);
         
-        return 'Insert data baru berhasil';
+        // return 'Insert data baru berhasil';
+
+        // $row = DB::table('m_kategori')->where('kategori_kode', 'SNK')->update(['kategori_nama' => 'Camilan']);
+        // return 'Update data berhasil. jumlah data yang diupdate: ' . $row. ' baris';
+
+        // $row = DB::table('m_kategori')->where('kategori_kode', 'SNK')->delete();
+        // return 'Delete data berhasil. jumlah data yang dihapus: ' . $row. ' baris';
+
+        $data = DB::select('select * from m_kategori');
+        return view('kategori', ['data' => $data]);
     }
 }
