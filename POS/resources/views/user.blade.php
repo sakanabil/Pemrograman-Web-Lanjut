3e06189760370b02c72ffd2f203f58df06c3371b
saka <sakanabil1221@gmail.com>
2025-03-16 17:00:26 +0700

    JS 4 Praktikum 2.7

--- POS/resources/views/user.blade.php
+++ POS/resources/views/user.blade.php
@@ -1,12 +1,14 @@
 <body>
     <h1>Data User</h1>
-    <a href="/Pemrograman-Web-Lanjut/POS/public/user/tambah">+ Tambah User</a>
+    <a href="user/tambah">+ Tambah User</a>
     <table border="1" cellpadding="2" cellspacing="0">
         <tr>
             <td>ID</td>
             <td>Username</td>
             <td>Nama</td>
             <td>ID Level Pengguna</td>
+            <td>Kode Level</td>
+            <td>Nama Level</td>
             <td>Aksi</td>
         </tr>
         @foreach ($data as $d)
@@ -15,10 +17,9 @@
             <td>{{ $d->username }}</td>
             <td>{{ $d->nama }}</td>
             <td>{{ $d->level_id }}</td>
-            <td>
-                <a href="/Pemrograman-Web-Lanjut/POS/public/user/ubah/{{ $d->user_id }}">Ubah</a> | 
-                <a href="/Pemrograman-Web-Lanjut/POS/public/user/hapus/{{ $d->user_id }}">Hapus</a>
-            </td>
+            <td>{{ $d->level->level_kode }}</td>
+            <td>{{ $d->level->level_nama }}</td>
+            <td><a href="user/ubah/{{ $d->user_id }}">Ubah</a> | <a href="user/hapus/{{ $d->user_id }}">Hapus</a></td>
         </tr>
         @endforeach
     </table>
