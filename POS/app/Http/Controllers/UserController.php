9f48fce9b600c354f37cd5efd2775edb68a8b07e
saka <sakanabil1221@gmail.com>
2025-03-23 20:30:19 +0700

    JS 6 Praktikum 2

--- POS/app/Http/Controllers/UserController.php
+++ POS/app/Http/Controllers/UserController.php
@@ -198,4 +198,53 @@ class UserController extends Controller
         }
         redirect('/');
     }
+
+    public function edit_ajax(string $id)
+    {
+        $user = UserModel::find($id);
+        $level = LevelModel::select('level_id', 'level_nama')->get();
+
+        return view('user.edit_ajax', ['user' => $user, 'level' => $level]);
+    }
+
+    public function update_ajax(Request $request, $id)
+    {
+        if($request->ajax() || $request->wantsJson()) {
+            $rules = [
+                'level_id'  => 'required|integer',
+                'username'  => 'required|max:20|unique:m_user,username,' . $id . ',user_id',
+                'nama'      => 'required|max:100',
+                'password'  => 'nullable|min:6|max:20'
+            ];
+
+            $validator = Validator::make($request->all(), $rules);
+
+            if ($validator->fails()) { 
+                return response()->json([ 
+                    'status'   => false,    // respon json, true: berhasil, false: gagal 
+                    'message'  => 'Validasi gagal.', 
+                    'msgField' => $validator->errors()  // menunjukkan field mana yang error 
+                ]); 
+            } 
+     
+            $check = UserModel::find($id); 
+            if ($check) { 
+                if(!$request->filled('password') ){ // jika password tidak diisi, maka hapus dari request 
+                    $request->request->remove('password'); 
+                } 
+                 
+                $check->update($request->all()); 
+                return response()->json([ 
+                    'status'  => true, 
+                    'message' => 'Data berhasil diupdate' 
+                ]); 
+            } else{ 
+                return response()->json([ 
+                    'status'  => false, 
+                    'message' => 'Data tidak ditemukan' 
+                ]); 
+            } 
+        } 
+        return redirect('/');
+    }
 }
