3955e605c12eb1367b9fbed6b7d823fb908029cf
saka <sakanabil1221@gmail.com>
2025-05-13 16:52:54 +0700

    JS 7 Praktikum 2

--- POS/routes/web.php
+++ POS/routes/web.php
@@ -26,6 +26,26 @@ Route::middleware(['auth'])->group(function () { // semua rute di dalam group in
 
     Route::get('/', [WelcomeController::class, 'index']);
 
+        // route level
+
+    // artinya semua route didalam group ini harus punya role ADM (admin)
+    Route::middleware(['authorize:ADM'])->group(function () {
+        Route::get('/', [LevelController::class, 'index']);
+        Route::post('/list', [LevelController::class, 'list']);
+        Route::get('/create', [LevelController::class, 'create']);
+        Route::post('/', [LevelController::class, 'store']);
+        Route::get('/create_ajax', [LevelController::class, 'create_ajax']);
+        Route::post('/ajax', [LevelController::class, 'store_ajax']);
+        Route::get('/{id}', [LevelController::class, 'show']);
+        Route::get('/{id}/edit', [LevelController::class, 'edit']);
+        Route::put('/{id}', [LevelController::class, 'update']);
+        Route::get('{id}/edit_ajax', [LevelController::class, 'edit_ajax']);
+        Route::put('/{id}/update_ajax', [LevelController::class, 'update_ajax']);
+        Route::get('{id}/delete_ajax', [LevelController::class, 'confirm_ajax']);
+        Route::delete('/{id}/delete_ajax', [LevelController::class, 'delete_ajax']);
+        Route::delete('/{id}', [LevelController::class, 'destroy'])->name('destroy');
+    });
+
     Route::group(['prefix' => 'user'], function () {
         Route::get('/', [UserController::class, 'index']);
         Route::post('/list', [UserController::class, 'list']);
