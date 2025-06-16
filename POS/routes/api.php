541c26a1b895f01775a89e164615a5258603a38f
saka <sakanabil1221@gmail.com>
2025-06-16 17:51:25 +0700

    JS 10 Praktikum 4

--- POS/routes/api.php
+++ POS/routes/api.php
@@ -4,6 +4,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\RegisterController;
 use App\Http\Controllers\Api\LoginController;
+use App\Http\Controllers\Api\LogoutController;
+use App\Http\Controllers\Api\LevelController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -22,3 +25,9 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::post('/logout', App\Http\Controllers\Api\LogoutController::class)->name('logout');
+
+Route::get('/levels', [LevelController::class, 'index']);
+Route::post('/levels', [LevelController::class, 'store']);
+Route::get('/levels/{level}', [LevelController::class, 'show']);
+Route::put('/levels/{level}', [LevelController::class, 'update']);
+Route::delete('/levels/{level}', [LevelController::class, 'destroy']);
